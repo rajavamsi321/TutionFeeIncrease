6c61671634426060c7f17df05be0f7b02fd2af12
RajaVamsi Chidella <rajavamsi.chidella@gmail.com>
2018-05-21 15:48:10 -0500

    Initial commit

---
+++ TutionFeeIncrease/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TutionFeeIncrease
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalTutFee = new System.Windows.Forms.Button();
+            this.lstBoxOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCalTutFee
+            // 
+            this.btnCalTutFee.Location = new System.Drawing.Point(260, 33);
+            this.btnCalTutFee.Name = "btnCalTutFee";
+            this.btnCalTutFee.Size = new System.Drawing.Size(161, 74);
+            this.btnCalTutFee.TabIndex = 0;
+            this.btnCalTutFee.Text = "Tution For Next Five Years";
+            this.btnCalTutFee.UseVisualStyleBackColor = true;
+            this.btnCalTutFee.Click += new System.EventHandler(this.btnCalTutFee_Click);
+            // 
+            // lstBoxOutput
+            // 
+            this.lstBoxOutput.FormattingEnabled = true;
+            this.lstBoxOutput.ItemHeight = 16;
+            this.lstBoxOutput.Location = new System.Drawing.Point(216, 137);
+            this.lstBoxOutput.Name = "lstBoxOutput";
+            this.lstBoxOutput.Size = new System.Drawing.Size(384, 228);
+            this.lstBoxOutput.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstBoxOutput);
+            this.Controls.Add(this.btnCalTutFee);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalTutFee;
+        private System.Windows.Forms.ListBox lstBoxOutput;
+    }
+}
+
---
+++ TutionFeeIncrease/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TutionFeeIncrease
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalTutFee_Click(object sender, EventArgs e)
+        {
+            const int TUITION_PER_SEM = 6000;
+            decimal result = TUITION_PER_SEM;
+            const decimal percentageIncrease = 0.02m;
+            lstBoxOutput.Items.Add("Year \tTution Fee Increased Per Year");
+            for (int i = 1; i <= 5; i++)
+            {
+                result += (result * percentageIncrease);
+                lstBoxOutput.Items.Add(i + "\t" +  result.ToString("C"));
+            }
+        }
+    }
+}
